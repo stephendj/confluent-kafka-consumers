829300c4e98f607d234763753e184739ac1aae71
Stephen Djohan <stephendj181194@gmail.com>
2019-06-05 23:37:40 +0700

    Change group id name

--- creditcardsettlements_consumer.py
+++ creditcardsettlements_consumer.py
@@ -48,7 +48,7 @@ if __name__ == '__main__':
         'security.protocol': 'SASL_SSL',
         'sasl.username': os.getenv('SASL_USERNAME'),
         'sasl.password': os.getenv('SASL_PASSWORD'),
-        'group.id': str(uuid.uuid1()),
+        'group.id': 'connect-sink-jdbc-xendit-deposit-service-creditcardsettlements',
         'auto.offset.reset': 'earliest',
         'schema.registry.url': os.getenv('SCHEMA_REGISTRY_URL')
     }
